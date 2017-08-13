28980763772def65416521e8a1fd90ab26fcfff8
Victor Allen <vamuigua@gmail.com>
2017-08-13 14:45:14 +0300

    Add routes for posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,26 @@
 class PostsController < ApplicationController
+
+	def index
+	end
+
+	def new
+		#create a new post in the form
+		@posts = Post.new
+	end
+
+	def create
+		@posts = Post.new(post_params)
+
+		if @post.save
+			redirect_to @post, notice:"Article was successfully saved!"
+		else
+			render 'new',notice:"Unable to save your post."
+		end
+	end
+
+	private
+
+	def post_params
+		params.require(:post).permit(:title,:content)
+	end
 end
