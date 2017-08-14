9bce39d74902d8e50ab8c8a85458b6cb986f38fc
Victor Allen <vamuigua@gmail.com>
2017-08-14 08:17:48 +0300

    Added CRUD methods for projects

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,5 +1,48 @@
 class ProjectsController < ApplicationController
-	def index
-		@projects = Project.all.order("created_at desc")
-	end
+
+  before_action :find_project, only:[:show,:edit,:update,:destroy]
+
+  def index
+    @projects = Project.all.order("created_at desc")
+  end
+
+  def create
+    @project = Project.new(project_params)
+
+    if @project.save
+      redirect_to @project, notice:"Project was successfully saved!"
+    else
+      render 'new',notice:"Unable to save your project."
+    end
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+    if @project.update(project_params)
+      redirect_to @project, notice:"Project was successfully saved!"
+    else
+      render 'edit', notice:"Unable to save your project."
+    end
+  end
+
+  def destroy
+    @project.destroy
+    redirect_to projects_path
+  end
+
+
+  private
+
+  def project_params
+    params.require(:project).permit(:title,:description,:link)
+  end
+
+  def find_project
+    @project = Project.find(params[:id])
+  end
 end
