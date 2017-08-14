623ae82e0aa6c70dac86a69ebb4ec41e46d170d2
Victor Allen <vamuigua@gmail.com>
2017-08-14 14:28:42 +0300

    Migrated slug to project

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170814094724) do
+ActiveRecord::Schema.define(version: 20170814112711) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -42,6 +42,8 @@ ActiveRecord::Schema.define(version: 20170814094724) do
     t.string "link"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "slug"
+    t.index ["slug"], name: "index_projects_on_slug", unique: true
   end
 
 end
