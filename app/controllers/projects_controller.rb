9a89edbca2cc2236d44ede9325d1844b0ab551a5
Victor Allen <vamuigua@gmail.com>
2017-08-14 10:26:20 +0300

    Added project route pages and methods

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -6,6 +6,10 @@ class ProjectsController < ApplicationController
     @projects = Project.all.order("created_at desc")
   end
 
+  def new
+  	@project = Project.new
+  end
+
   def create
     @project = Project.new(project_params)
 
