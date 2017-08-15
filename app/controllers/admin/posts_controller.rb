fbfb1f5791c2e03fc643c89e0b94291076ae0f32
Victor Allen <vamuigua@gmail.com>
2017-08-15 23:07:39 +0300

    Added posts controller for an admin

---
+++ app/controllers/admin/posts_controller.rb
@@ -0,0 +1,51 @@
+class Admin::PostsController < AdminController
+	before_action :find_post, only:[:show,:edit,:update,:destroy]
+
+	def index
+    @posts = Post.all.order("created_at desc").paginate(:page => params[:page], :per_page => 3)
+  end
+
+  def new
+    #create a new post in the form
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+
+    if @post.save
+      redirect_to admin_post_path(@post), notice:"Article was successfully saved!"
+    else
+      render 'new',alert:"Unable to save your post."
+    end
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+    if @post.update(post_params)
+      redirect_to admin_post_path(@post), notice:"Article was successfully saved!"
+    else
+      render 'edit', alert:"Unable to save your article."
+    end
+  end
+
+  def destroy
+  	@post.destroy
+  	redirect_to admin_posts_path,alert:"Successfully deleted post"
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title,:content,:slug)
+  end
+
+  def find_post
+    @post = Post.friendly.find(params[:id])
+  end
+end
