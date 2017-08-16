26998105f4e952ee0280b1f39f99867f1eb2e8be
Victor Allen <vamuigua@gmail.com>
2017-08-16 11:12:28 +0300

    Added admin projects controller methods

---
+++ app/controllers/admin/projects_controller.rb
@@ -0,0 +1,51 @@
+class Admin::ProjectsController < AdminController
+  before_action :find_projects, only:[:show,:edit,:update,:destroy]
+
+  def index
+    @projects = Project.all.order("created_at desc").paginate(:page => params[:page], :per_page => 3)
+  end
+
+  def new
+    #create a new project in the form
+    @project = Project.new
+  end
+
+  def create
+    @project = Project.new(project_params)
+
+    if @project.save
+      redirect_to admin_project_path(@project), notice:"Project was successfully saved!"
+    else
+      render 'new',alert:"Unable to save your project."
+    end
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+    if @project.update(project_params)
+      redirect_to admin_project_path(@project), notice:"Project was successfully saved!"
+    else
+      render 'edit', alert:"Unable to save your project."
+    end
+  end
+
+  def destroy
+    @project.destroy
+    redirect_to admin_projects_path,alert:"Successfully deleted project"
+  end
+
+  private
+
+  def project_params
+    params.require(:project).permit(:title,:description,:link,:slug)
+  end
+
+  def find_project
+    @project = Project.friendly.find(params[:id])
+  end
+end
