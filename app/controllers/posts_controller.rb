d3c4affb9a08657c751c662d0bd5ffdd644abc2c
Victor Allen <vamuigua@gmail.com>
2017-08-16 14:44:48 +0300

    Removed projects and posts routes for user

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,44 +1,13 @@
 class PostsController < ApplicationController
 
-  before_action :find_post, only:[:show,:edit,:update,:destroy]
+  before_action :find_post, only:[:show]
   before_action :authenticate_user!, except:[:index,:show]
 
   def index
     @posts = Post.all.order("created_at desc").paginate(:page => params[:page], :per_page => 3)
   end
 
-  def new
-    #create a new post in the form
-    @post = Post.new
-  end
-
-  def create
-    @post = Post.new(post_params)
-
-    if @post.save
-      redirect_to @post, notice:"Article was successfully saved!"
-    else
-      render 'new',alert:"Unable to save your post."
-    end
-  end
-
   def show
-  end
-
-  def edit
-  end
-
-  def update
-    if @post.update(post_params)
-      redirect_to @post, notice:"Article was successfully saved!"
-    else
-      render 'edit', alert:"Unable to save your article."
-    end
-  end
-
-  def destroy
-  	@post.destroy
-  	redirect_to posts_path
   end
 
   private
--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,45 +1,14 @@
 class ProjectsController < ApplicationController
 
-  before_action :find_project, only:[:show,:edit,:update,:destroy]
+  before_action :find_project, only:[:show]
   before_action :authenticate_user!, except:[:index,:show]
 
   def index
     @projects = Project.all.order("created_at desc").paginate(:page => params[:page], :per_page => 3)
   end
 
-  def new
-    @project = Project.new
-  end
-
-  def create
-    @project = Project.new(project_params)
-
-    if @project.save
-      redirect_to @project, notice:"Project was successfully saved!"
-    else
-      render 'new',notice:"Unable to save your project."
-    end
-  end
-
   def show
   end
-
-  def edit
-  end
-
-  def update
-    if @project.update(project_params)
-      redirect_to @project, notice:"Project was successfully saved!"
-    else
-      render 'edit', notice:"Unable to save your project."
-    end
-  end
-
-  def destroy
-    @project.destroy
-    redirect_to projects_path
-  end
-
 
   private
 
