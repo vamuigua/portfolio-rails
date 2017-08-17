c54d186b36ea471b3fdeed8c3899ef0730967ea5
Victor Allen <vamuigua@gmail.com>
2017-08-17 11:04:04 +0300

    Added polymorphism assosiation for making comments commentable

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,2 +1,5 @@
 class Comment < ApplicationRecord
+  #comments having many comments as commentable
+  #and belonging dynamicaly to other models using polymorphism
+  belongs_to :commentable, polymorphic: true
 end
