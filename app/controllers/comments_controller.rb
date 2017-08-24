7d673076b31f5839ed237d12af5c118a04ab0dc1
Victor Allen <vamuigua@gmail.com>
2017-08-24 12:57:04 +0300

    Fixed adding and deleting of referals and comments using ajax

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,26 +1,32 @@
 class CommentsController < ApplicationController
   before_action :load_commentable
   before_action :authenticate_user!, except:[:index,:show]
-
   def index
     @comments = @commentable.comments
   end
-
   def new
     @comment = @commentable.comments.new
   end
-
   def create
     @comment = @commentable.comments.create(params[:comment].permit(:content))
     if @comment.save
-      redirect_to @commentable, notice: "Comment created."
+      respond_to do |format|
+        format.html{redirect_to @commentable, notice: "Comment created."}
+        format.js
+      end
     else
       render :new
     end
   end
-
+  def destroy
+    @comment = @commentable.comments.find(params[:id])
+    @comment.destroy
+    respond_to do |format|
+      format.html{redirect_to posts_path}
+      format.js
+    end
+  end
   private
-
   def load_commentable
     resource, id = request.path.split('/')[1,2] #posts/1
     @commentable = resource.singularize.classify.constantize.find(id)
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,4 +2,5 @@ class Comment < ApplicationRecord
   #comments having many comments as commentable
   #and belonging dynamicaly to other models using polymorphism
   belongs_to :commentable, polymorphic: true
+  has_many :comments, as: :commentable
 end
