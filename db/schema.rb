baf4aced1b46d445a19b753c3e27c80fa460c057
Victor Allen <vamuigua@gmail.com>
2017-09-17 20:47:39 +0300

    Updated schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170822094016) do
+ActiveRecord::Schema.define(version: 20170917124432) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -43,6 +43,7 @@ ActiveRecord::Schema.define(version: 20170822094016) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "slug"
+    t.string "postimage"
     t.index ["slug"], name: "index_posts_on_slug", unique: true
   end
 
