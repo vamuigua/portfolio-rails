d1323f864b6c48e2c99e722ac20533726f2068f2
Victor Allen <vamuigua@gmail.com>
2017-09-18 06:57:56 +0300

    Added property for dependand on destroy to comments

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,5 @@
 class Post < ApplicationRecord
   mount_uploader :postimage, PostimageUploader
   #post having many comments as commentale
-  has_many :comments, as: :commentable
+  has_many :comments, as: :commentable, dependent: :destroy
 end
