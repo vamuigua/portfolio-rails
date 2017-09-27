9d3e9aa2eb733f5337b66087806cfc7a259da528
Victor Allen <vamuigua@gmail.com>
2017-09-27 17:40:26 +0300

    Removed comments authentication

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,6 +1,5 @@
 class CommentsController < ApplicationController
   before_action :load_commentable
-  before_action :authenticate_user!, except:[:index,:show]
   def index
     @comments = @commentable.comments
   end
